9606e9ac4d936d44115121613767ff36fc03095a
MakersMakingChange <makersmakingchange@gmail.com>
2018-10-20 22:09:56 -0700

    Updated the firmware to Version 1.11 *Added new buttons for XAC *Added shift functionality for XAC using 2-second long sip

--- Software/GameStik_Firmware/GameStik_Firmware.ino
+++ Software/GameStik_Firmware/GameStik_Firmware.ino
@@ -13,7 +13,7 @@
 */
 
 //Developed by : MakersMakingChange
-//VERSION: 1.1 (15 October 2018) 
+//VERSION: 1.11 (20 October 2018) 
 
 
 #include <EEPROM.h>
@@ -31,7 +31,7 @@
 #define TRANS_CONTROL A3                          // Unused Transistor Control Pin - digital output pin A3
 #define PIO4 A4                                   // Unused PIO4 Command Pin - digital output pin A4
 
-#define PRESSURE_JOYSTICK A5                        // Sip & Puff Pressure Transducer Pin - analog input pin A5
+#define PRESSURE_JOYSTICK A5                      // Sip & Puff Pressure Transducer Pin - analog input pin A5
 #define X_DIR_HIGH A0                             // X Direction High (Cartesian positive x : right) - analog input pin A0
 #define X_DIR_LOW A1                              // X Direction Low (Cartesian negative x : left) - digital output pin A1
 #define Y_DIR_HIGH A2                             // Y Direction High (Cartesian positive y : up) - analog input pin A2
@@ -44,9 +44,9 @@ Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
   true, true, false, 
   false, false, false,
   false, false, 
-  false, false, false);                         //Defining the joystick REPORT_ID and type
+  false, false, false);                         //Defining the joystick REPORT_ID and profile type
 
-int button1 = 0;
+int button1 = 0;                                //Declare button number variables 
 int button2 = 1;
 int button3 = 2;
 int button4 = 3;
@@ -55,22 +55,19 @@ int button6 = 5;
 int button7 = 6;
 int button8 = 7;
 
-int x_h, y_h, x_l, y_l;   
+int xHigh, yHigh, xLow, yLow;   
 
-int speed_counter = 2;
-int fixed_delay = 10;
+int speedCounter = 2;                         //Declare variables for speed functionality 
+int fixedDelay = 10;
+int joystickDelay;
 
-int joystick_delay;
+float sipThreshold;                           //Declare sip and puff variables 
+float puffThreshold;
+float joystickPress;
+unsigned int puffCount;
+unsigned int sipCount;
 
-float sip_threshold;
-float puff_threshold;
-float joystick_press;
-float joystick_back;
-
-unsigned int puff_count;
-unsigned int sip_count;
-
-int last_button_state[8];       // Last state of the button
+int lastButtonState[8];                       //Last state of the button
 
 //-----------------------------------------------------------------------------------------------------------------------------------
 
@@ -78,24 +75,25 @@ int last_button_state[8];       // Last state of the button
 
 void setup() {
 
-  Serial.begin(115200);                           // setting baud rate for serial coms for diagnostic data return from microcontroller
-  Serial1.begin(115200);                          // setting baud rate for Bluetooth module
+  Serial.begin(115200);                           //Set baud rate for serial coms for diagnostic data return from microcontroller
+  Serial1.begin(115200);                          
 
-  pinMode(LED_1, OUTPUT);                         // visual feedback #1
-  pinMode(LED_2, OUTPUT);                         // visual feedback #2
-  pinMode(TRANS_CONTROL, OUTPUT);                 // transistor pin output
-  pinMode(PIO4, OUTPUT);                          // command mode pin output
+  pinMode(LED_1, OUTPUT);                         //Set the visual feedback #1 LED pin to output mode
+  pinMode(LED_2, OUTPUT);                         //Set the visual feedback #2 LED pin to output mode
+  pinMode(TRANS_CONTROL, OUTPUT);                 //Set the transistor pin to output mode
+  pinMode(PIO4, OUTPUT);                          //Set the unused pin to output mode
   
-  pinMode(PRESSURE_JOYSTICK, INPUT);                // pressure sensor pin input
-  pinMode(X_DIR_HIGH, INPUT);                     // redefine the pins when all has been finalized
-  pinMode(X_DIR_LOW, INPUT);                      // ditto above
-  pinMode(Y_DIR_HIGH, INPUT);                     // ditto above
-  pinMode(Y_DIR_LOW, INPUT);                      // ditto above
+  pinMode(PRESSURE_JOYSTICK, INPUT);              //Set the pressure sensor pin to input mode
+  
+  pinMode(X_DIR_HIGH, INPUT);                     //Set the FSR pin to input mode
+  pinMode(X_DIR_LOW, INPUT);
+  pinMode(Y_DIR_HIGH, INPUT);
+  pinMode(Y_DIR_LOW, INPUT);
 
-  pinMode(PUSH_BUTTON_UP, INPUT_PULLUP);          // increase joystick speed button
-  pinMode(PUSH_BUTTON_DOWN, INPUT_PULLUP);        // decrease joystick speed button
+  pinMode(PUSH_BUTTON_UP, INPUT_PULLUP);          //Set the increase joystick speed pin to input mode with pullup
+  pinMode(PUSH_BUTTON_DOWN, INPUT_PULLUP);        //Set the decrease joystick speed pin to input mode with pullup
 
-  pinMode(2, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);                       //Set the unused pins to input mode with pullups
   pinMode(3, INPUT_PULLUP);
   pinMode(9, INPUT_PULLUP);
   pinMode(11, INPUT_PULLUP);
@@ -106,35 +104,35 @@ void setup() {
   // Initialize Joystick Library
   Joystick.begin();
   delay(10);
-  Pressure_Sensor_Initialization();
+  pressureSensorInitialization();               //Initialize the pressure sensor
   delay(10);
 
   //Initialize the last state of buttons
-  last_button_state[0] = 0;
-  last_button_state[1] = 0;
-  last_button_state[2] = 0;
-  last_button_state[3] = 0;
-  last_button_state[4] = 0;
-  last_button_state[5] = 0;
-  last_button_state[6] = 0;
-  last_button_state[7] = 0;
+  lastButtonState[0] = 0;
+  lastButtonState[1] = 0;
+  lastButtonState[2] = 0;
+  lastButtonState[3] = 0;
+  lastButtonState[4] = 0;
+  lastButtonState[5] = 0;
+  lastButtonState[6] = 0;
+  lastButtonState[7] = 0;
 
-  Joystick_Speed_Value();                         // reads saved joystick speed parameter from EEPROM
+  joystickSpeedValue();                         //Reads saved joystick speed parameter from EEPROM and sets the speed counter
   delay(10);
-  int exec_time = millis();
+  
+  int execTime = millis();
   Serial.print("Configuration time: ");
-  Serial.println(exec_time);
+  Serial.println(execTime);
 
-  blink(4, 250, 3);                               // end initialization visual feedback
+  blink(4, 250, 3);                             //End the initialization visual feedback
 
-  Display_Feature_List();  
-  joystick_delay = pow(1.6,(9-speed_counter))*fixed_delay;
+  displayFeatureList();                         //Display the list of features 
+  
+  joystickDelay = pow(1.6,(9-speedCounter))*fixedDelay;
 
   Serial.print("Speed level: ");
-  Serial.println((9-speed_counter));
+  Serial.println((9-speedCounter));
   delay(5);
-
-
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------
@@ -142,43 +140,43 @@ void setup() {
 //***START OF INFINITE LOOP***//
 
 void loop() {
-  x_h = analogRead(X_DIR_HIGH);
-  x_l = analogRead(X_DIR_LOW);
-  y_h = analogRead(Y_DIR_LOW);
-  y_l = analogRead(Y_DIR_HIGH);
+  xHigh = analogRead(X_DIR_HIGH);
+  xLow = analogRead(X_DIR_LOW);
+  yHigh = analogRead(Y_DIR_LOW);
+  yLow = analogRead(Y_DIR_HIGH);
 
-  x_h = map(x_h, 0, 1023, 0, 16);
-  x_l = map(x_l, 0, 1023, 0, 16);
-  y_h = map(y_h, 0, 1023, 0, 16);
-  y_l = map(y_l, 0, 1023, 0, 16);
+  xHigh = map(xHigh, 0, 1023, 0, 16);                 //Map x and y values from (0 to 1023) bound to (0 to 16) as target bound
+  xLow = map(xLow, 0, 1023, 0, 16);                   //The actual input values are approximately in (0 to 800) bound range
+  yHigh = map(yHigh, 0, 1023, 0, 16);
+  yLow = map(yLow, 0, 1023, 0, 16);
 
-  int xx_tmp = x_h - x_l;
-  int yy_tmp = y_h - y_l;
+  int xDelta = xHigh - xLow;                        //Calculate the x and y delta values   
+  int yDelta = yHigh - yLow;
+ 
+  int xx = (xDelta >= 0)? sq(xDelta):-sq(xDelta);     //Square the magnitude of x and y Delta values
+  int yy = (yDelta >= 0)? sq(yDelta):-sq(yDelta);
   
-  int xx = (xx_tmp >= 0)? sq(xx_tmp):-sq(xx_tmp);
-  int yy = (yy_tmp >= 0)? sq(yy_tmp):-sq(yy_tmp);
-  xx -= (xx_tmp >= 0)? int(sqrt(yy_tmp)):-int(sqrt(-yy_tmp));
-  yy -= (yy_tmp >= 0)? int(sqrt(xx_tmp)):-int(sqrt(-xx_tmp));
+  xx -= (xDelta >= 0)? int(sqrt(yDelta)):-int(sqrt(-yDelta));   //Subtract the square root of y Delta value from x Delta value to make movement smoother 
+  yy -= (yDelta >= 0)? int(sqrt(xDelta)):-int(sqrt(-xDelta));   //Subtract the square root of x Delta value from y Delta value to make movement smoother 
 
-  xx = constrain(xx, -128, 128);
+  xx = constrain(xx, -128, 128);          //Put constrain to set x and y range between -128 and 128 as lower and upper bounds 
   yy = constrain(yy, -128, 128);
-
-  xx = map(xx, -128, 128, 0, 1023);
+  
+  xx = map(xx, -128, 128, 0, 1023);       //Map back x and y range from (-128 to 128) as current bounds to (0 to 1023) as target bounds
   yy = map(yy, -128, 128, 0, 1023);
 
-  Joystick.setXAxis(xx);
+  Joystick.setXAxis(xx);                  //Set Joystick's x and y values
   Joystick.setYAxis(yy);
+  delay(joystickDelay);
   
-  delay(joystick_delay);
-  
-  //joystick speed control push button functions below
+  //The joystick speed control push button functions
 
   if (digitalRead(PUSH_BUTTON_UP) == LOW) {
     delay(250);
     if (digitalRead(PUSH_BUTTON_DOWN) == LOW) {
       //Additional function 
     } else {
-      Increase_Joystick_Speed();      // increase joystick speed with push button up
+      increaseJoystickSpeed();      //The increase joystick speed with push button up
     }
   }
 
@@ -187,71 +185,90 @@ void loop() {
     if (digitalRead(PUSH_BUTTON_UP) == LOW) {
       //Additional function 
     } else {
-      Decrease_Joystick_Speed();      // decrease joystick speed with push button down
+      decreaseJoystickSpeed();      //The decrease joystick speed with push button down
     }
   }
 
-  //pressure sensor sip and puff functions below
+  //Pressure sensor sip and puff functions
 
-  joystick_press = (((float)analogRead(PRESSURE_JOYSTICK)) / 1023.0) * 5.0;
-
-  if (joystick_press < puff_threshold) {
-    while (joystick_press < puff_threshold) {
-      joystick_press = (((float)analogRead(PRESSURE_JOYSTICK)) / 1023.0) * 5.0;
-      puff_count++;                                                             //Threshold counter
+  joystickPress = (((float)analogRead(PRESSURE_JOYSTICK)) / 1023.0) * 5.0;   
+  //Measure the pressure value and compare the result with puff pressure Thresholds 
+  if (joystickPress < puffThreshold) {
+    while (joystickPress < puffThreshold) {
+      joystickPress = (((float)analogRead(PRESSURE_JOYSTICK)) / 1023.0) * 5.0;
+      puffCount++;                  //Threshold counter
       delay(5);
     }
-      if (puff_count < 150) {
+      if (puffCount < 150) {
         //Press joystick button number 5 or button A/X in XAC
-        if (!last_button_state[4]) {
+        if (!lastButtonState[4]) {
           Joystick.pressButton(button5);
           delay(250);
           Joystick.releaseButton(button5);
           delay(50);
-          last_button_state[0] = 0;
+          lastButtonState[4] = 0;
         }
-      } else if (puff_count > 150 && puff_count < 500) {
-        //Press joystick button number 7 or button View in XAC
-        if (!last_button_state[6]) {
+      } else if (puffCount > 150 && puffCount < 450) {
+        //Press joystick button number 4 or button LB/RB in XAC
+        if (!lastButtonState[3]) {
+          Joystick.pressButton(button4);
+          delay(250);
+          Joystick.releaseButton(button4);
+          delay(50);
+          lastButtonState[3] = 0;
+        } 
+      } else if (puffCount > 450) {
+        //Press joystick button number 7 or button View/X1 in XAC
+        if (!lastButtonState[6]) {
           Joystick.pressButton(button7);
           delay(250);
           Joystick.releaseButton(button7);
           delay(50);
-          last_button_state[2] = 0;
+          lastButtonState[6] = 0;
         } 
-      } 
-
-    puff_count = 0;
+      }
+    puffCount = 0;
   }
-
-  if (joystick_press > sip_threshold) {
-    while (joystick_press > sip_threshold) {
-      joystick_press = (((float)analogRead(PRESSURE_JOYSTICK)) / 1023.0) * 5.0;
-      sip_count++;                                                                //Threshold counter
+  //Measure the pressure value and compare the result with sip pressure Thresholds 
+  if (joystickPress > sipThreshold) {
+    while (joystickPress > sipThreshold) {
+      joystickPress = (((float)analogRead(PRESSURE_JOYSTICK)) / 1023.0) * 5.0;
+      sipCount++;                //Threshold counter
       delay(5);
     }
-      if (sip_count < 150) {
+      if (sipCount < 150) {
         //Press joystick button number 6 or button B/Y in XAC
-        if (!last_button_state[5]) {
+        if (!lastButtonState[5]) {
           Joystick.pressButton(button6);
           delay(250);
           Joystick.releaseButton(button6);
           delay(50);
-          last_button_state[1] = 0;
+          lastButtonState[5] = 0;
         } 
-      } else if (sip_count > 150 && sip_count < 500) {
-        //Press joystick button number 8 or button Menu/X1 in XAC
-         if (!last_button_state[7]) {
+      } else if (sipCount > 150 && sipCount < 450) {
+        //Press joystick button number 1 or button X1/View in XAC ( Used for Shift action )
+        blink(1, 250, 1); 
+        if (!lastButtonState[0]) {
+          Joystick.pressButton(button1);
+          delay(50);
+          lastButtonState[0] = 1;
+        } 
+        else {
+          Joystick.releaseButton(button1);
+          delay(50);  
+          lastButtonState[0] = 0;    
+        }
+      } else if (sipCount > 450) {
+        //Press joystick button number 8 or button Menu/X2 in XAC
+         if (!lastButtonState[7]) {
           Joystick.pressButton(button8);
           delay(250);
           Joystick.releaseButton(button8);
           delay(50);
-          last_button_state[3] = 0;
+          lastButtonState[7] = 0;
         } 
-      } else {
-        delay(5);
       }
-    sip_count = 0;
+    sipCount = 0;
   }
   
 }
@@ -260,14 +277,15 @@ void loop() {
 
 //-----------------------------------------------------------------------------------------------------------------------------------
 
+//***DISPLAY FEATURE LIST FUNCTION***//
 
-void Display_Feature_List(void) {
+void displayFeatureList(void) {
 
   Serial.println(" ");
   Serial.println(" --- ");
   Serial.println("This is the GameStik firmware");
   Serial.println(" ");
-  Serial.println("VERSION: 1.1 (15 October 2018)");
+  Serial.println("VERSION: 1.11 (20 October 2018)");
   Serial.println(" ");
   Serial.println(" --- ");
   Serial.println(" ");
@@ -275,99 +293,103 @@ void Display_Feature_List(void) {
 }
 
 
-//***LED BLINK FUNCTIONS***//
+//***LED BLINK FUNCTION***//
 
-void blink(int num_blinks, int delay_blinks, int led_number ) {
-  if (num_blinks < 0) num_blinks *= -1;
+void blink(int numBlinks, int delayBlinks, int ledNumber ) {
+  if (numBlinks < 0) numBlinks *= -1;
 
-  switch (led_number) {
+  switch (ledNumber) {
     case 1: {
-        for (int i = 0; i < num_blinks; i++) {
+        for (int i = 0; i < numBlinks; i++) {
           digitalWrite(LED_1, HIGH);
-          delay(delay_blinks);
+          delay(delayBlinks);
           digitalWrite(LED_1, LOW);
-          delay(delay_blinks);
+          delay(delayBlinks);
         }
         break;
       }
     case 2: {
-        for (int i = 0; i < num_blinks; i++) {
+        for (int i = 0; i < numBlinks; i++) {
           digitalWrite(LED_2, HIGH);
-          delay(delay_blinks);
+          delay(delayBlinks);
           digitalWrite(LED_2, LOW);
-          delay(delay_blinks);
+          delay(delayBlinks);
         }
         break;
       }
     case 3: {
-        for (int i = 0; i < num_blinks; i++) {
+        for (int i = 0; i < numBlinks; i++) {
           digitalWrite(LED_1, HIGH);
-          delay(delay_blinks);
+          delay(delayBlinks);
           digitalWrite(LED_1, LOW);
-          delay(delay_blinks);
+          delay(delayBlinks);
           digitalWrite(LED_2, HIGH);
-          delay(delay_blinks);
+          delay(delayBlinks);
           digitalWrite(LED_2, LOW);
-          delay(delay_blinks);
+          delay(delayBlinks);
         }
         break;
       }
   }
 }
 
-//***HID JOYSTICK SPEED FUNCTIONS***//
+//***HID JOYSTICK SPEED FUNCTION***//
 
-void Joystick_Speed_Value(void) {
+void joystickSpeedValue(void) {
   int var;
   EEPROM.get(2, var);
   delay(5);
-  speed_counter = var;
+  speedCounter = var;
 }
 
-void Increase_Joystick_Speed(void) {
-  speed_counter++;
+//***INCREASE JOYSTICK SPEED FUNCTION***//
 
-  if (speed_counter == 9) {
+void increaseJoystickSpeed(void) {
+  speedCounter++;
+
+  if (speedCounter == 9) {
     blink(6, 50, 3);
-    speed_counter = 8;
+    speedCounter = 8;
   } else {
-    blink(speed_counter, 100, 1);
-    joystick_delay = pow(1.6,(9-speed_counter))*fixed_delay;
-    EEPROM.put(2, speed_counter);
+    blink(speedCounter, 100, 1);
+    joystickDelay = pow(1.6,(9-speedCounter))*fixedDelay;
+    EEPROM.put(2, speedCounter);
     delay(25);
   }
   Serial.print("Speed level: ");
-  Serial.println((9-speed_counter));
+  Serial.println((9-speedCounter));
 }
 
-void Decrease_Joystick_Speed(void) {
-  speed_counter--;
+//***DECREASE JOYSTICK SPEED FUNCTION**//
 
-  if (speed_counter == -1) {
+void decreaseJoystickSpeed(void) {
+  speedCounter--;
+
+  if (speedCounter == -1) {
     blink(6, 50, 3);     // twelve very fast blinks
-    speed_counter = 0;
-  } else if (speed_counter == 0) {
+    speedCounter = 0;
+  } else if (speedCounter == 0) {
     blink(1, 350, 1);
-    joystick_delay = pow(1.6,(9-speed_counter))*fixed_delay;
-    EEPROM.put(2, speed_counter);
+    joystickDelay = pow(1.6,(9-speedCounter))*fixedDelay;
+    EEPROM.put(2, speedCounter);
     delay(25);
   } else {
-    blink(speed_counter, 100, 1);
-    joystick_delay = pow(1.6,(9-speed_counter))*fixed_delay;
-    EEPROM.put(2, speed_counter);
+    blink(speedCounter, 100, 1);
+    joystickDelay = pow(1.6,(9-speedCounter))*fixedDelay;
+    EEPROM.put(2, speedCounter);
     delay(25);
   }
   Serial.print("Speed level: ");
-  Serial.println((9-speed_counter));
+  Serial.println((9-speedCounter));
 }
 
 //***PRESSURE SENSOR INITIALIZATION FUNCTION***//
 
-void Pressure_Sensor_Initialization(void) {
+void pressureSensorInitialization(void) {
   
   float nominal_joystick_value = (((float)analogRead(PRESSURE_JOYSTICK)) / 1024.0) * 5.0; // Initial neutral pressure transducer analog value [0.0V - 5.0V]
-  sip_threshold = nominal_joystick_value + 0.5;    //Create sip pressure threshold value ***Larger values tend to minimize frequency of inadvertent activation
-  puff_threshold = nominal_joystick_value - 0.5;   //Create puff pressure threshold value ***Larger values tend to minimize frequency of inadvertent activation
+  sipThreshold = nominal_joystick_value + 0.5;    //Create sip pressure threshold value ***Larger values tend to minimize frequency of inadvertent activation
+  puffThreshold = nominal_joystick_value - 0.5;   //Create puff pressure threshold value ***Larger values tend to minimize frequency of inadvertent activation
   
 }
 
